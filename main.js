40145521e7aac278227c4edb6a78c265d71a3d2b
YasserDalali <yasser.dalali.personal@gmail.com>
2024-03-30 23:20:02 +0100

    added card front and back UI, added a flip animation using JS

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function flip() {
+document.querySelector('.card').classList.toggle('flipped');
+  document.querySelector('.card').classList.toggle('card-back');
+  document.querySelector('.name').classList.toggle('hidden')
+  document.querySelector('.date').classList.toggle('hidden')
+  document.querySelector('.logo').classList.toggle('hidden')
+  document.querySelector('.number').classList.toggle('hidden')
+  document.querySelector('.cvc').classList.toggle('hidden')
+  };
